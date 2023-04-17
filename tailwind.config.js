342e07ce80d218df842afd5d66064271c26586ce
Angel Boada <aboadasalazar@gmail.com>
2023-04-17 09:58:34 -0500

    Avance proyecto YT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        '10': 'repeat(10, minmax(0, 1fr))',
+      },
+    },
+    plugins: [],
+  }
 }
